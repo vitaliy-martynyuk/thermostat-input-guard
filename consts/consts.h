dd2fda11e8eca0a33b77010bb3d6b61e4fe8770f
vitaliy-martynyuk <vitaliymartiniuk16@gmail.com>
2026-09-09 19:18:30 +0300

    make globals inline

--- consts/consts.h
+++ consts/consts.h
@@ -9,16 +9,16 @@ namespace consts
 
 	namespace modeLabel
 	{
-		constexpr string_view heat{ "Heat" };
-		constexpr string_view cool{ "Cool" };
-		constexpr string_view fan{ "Fan" };
+		inline constexpr string_view heat{ "Heat" };
+		inline constexpr string_view cool{ "Cool" };
+		inline constexpr string_view fan{ "Fan" };
 	}
 
 	namespace modeCode
 	{
-		constexpr char heat{ 'H' };
-		constexpr char cool{ 'C' };
-		constexpr char fan{ 'F' };
+		inline constexpr char heat{ 'H' };
+		inline constexpr char cool{ 'C' };
+		inline constexpr char fan{ 'F' };
 	}
 }
 
